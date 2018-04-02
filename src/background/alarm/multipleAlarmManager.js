003c6483458c0361fd0fe3d90a9edaaa83d8837c
Albert Patterson <apatterson@truefit.com>
2018-04-02 19:45:21 -0400

    refactoring alarms and mamager

--- src/background/alarm/multipleAlarmManager.js
+++ src/background/alarm/multipleAlarmManager.js
@@ -5,12 +5,10 @@ module.exports = {
   alarms: [],
   add: function(alarmData){
     alarms.push(new DailyAlarmWithWarning(alarmData, this.onTryStartWarning, this.onTryStopWarning, this.onTryStartAlarm, this.onTryStopAlarm));
-    stateCounts.none++;
   },
   remove: function(idx){
     alarms[idx].clear();
     alarms[idx]=null;
-    stateCounts[alarm[idx].state]--;
   },
   onTryStartWarning(){
     if(getStateCounts().warning===0) this.doStartWarning();
@@ -24,21 +22,10 @@ module.exports = {
   onTryStopAlarm(){
     if(getStateCounts().alarm===1) this.doStopAlarm();
   },
-  getOverallState(){
-    return alarms.reduce((overallState, alarm)=>{
-      if(overallState==="alarm" || alarm.state==="alarm"){
-        return "alarm";
-      }else if(overallState==="warning" || alarm.state==="warning"){
-        return "warning";
-      }else{
-        return "none";
-      }
-    }, {state: "none"})
-  },
   getStateCounts(){
     stateCounts = {alarm: 0, warning: 0, none: 0};
     alarms.foreach((alarm) => {
-      stateCounts[alarm.state]++
+      stateCounts[alarm.state]++;
     });
     return stateCounts;
   }
